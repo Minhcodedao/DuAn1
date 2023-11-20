9a6fd3dd8b84e2846c0a528d2b7d2be876ab0b5a
tiendat021020042009 <tiendat0416@gmail.com>
2023-11-20 20:22:13 +0700

    8:22 pm

---
+++ footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="vi">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <link rel="stylesheet" href="./css/sto.css" />
+  <body>
+
+    <!------------------Footer-->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="footer-navigation">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="footer-social-media">
+          <!-- Add your social media links here -->
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+        <div class="footer-contact-info">
+          <p>Address: FPT Polytechinic Trịnh Văn Bô Nam Từ Liêm Hà Nội</p>
+          <br />
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.863668699041!2d105.74467967558725!3d21.038140280613522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313455e940879933%3A0xcf10b34e9f1a03df!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1699960322123!5m2!1svi!2s"
+              width="200"
+              height="130"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <p>Call: 0377961716</p>
+          <p>Email: nhom6trangsuc@fpt.edu.vn</p>
+        </div>
+        <div class="footer-copy-right">
+          <p>&copy; 2023 Your Company nhom6. All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
