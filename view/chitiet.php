6c6b452c6d6295ccb42d92638df05ba1153a8185
tiendat021020042009 <tiendat0416@gmail.com>
2023-11-22 15:28:11 +0700

    ok

--- view/chitiet.php
+++ view/chitiet.php
@@ -54,46 +54,46 @@
       <div class="camket">
         <div class="ck_left">
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
+            <img src="./image/icv-titleh3.png" height="40px" width=" 40px">
             <h3>CAM KẾT CHẤT LƯỢNG</h3>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
+            <img src="./image/icon_tix.png" alt="">
             <p>Thương hiệu trang sức cưới thuộc Tập đoàn Vàng bạc đá quý DOJI</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>100% Chuẩn xác về hàm lượng. Tất cả các sản phẩm trang sức được kiểm định chặt chẽ với máy quang phổ, cam kết chuẩn xác hàm lượng.</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>100% chuẩn xác về trọng lượng.</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>Mọi sản phẩm trang sức bán ra đều có hóa đơn, đầy đủ thông tin về hàm lượng, trọng lượng và điều kiện bảo hành.</p>
           </div>
         </div>
         <div class="ck_right">
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <h3>CAM KẾT CHẤT LƯỢNG</h3>
+            <img src="./image/icvtitlth3.png" height="40px" width=" 40px">
+            <h3>HƯỚNG DẪN MUA HÀNG ONLINE</h3>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>Thương hiệu trang sức cưới thuộc Tập đoàn Vàng bạc đá quý DOJI</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>Lựa chọn sản phẩm tại các chuyên mục trên website</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>Thêm vào giỏ hàng.</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>Kiểm tra thông tin đơn hàng và đặt hàng</p>
           </div>
           <div class="candong">
-            <img src="./image/icv-titleh3.png" alt="">
-            <p>CAM KẾT CHẤT LƯỢNG</p>
+            <img src="./image/icon_tix.png" alt="">
+            <p>Kiểm tra và xác nhận đơn hàng</p>
           </div>
         </div>
       </div>
