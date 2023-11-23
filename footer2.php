ad5cf35969249b8929c65fc6c3c9c235b156cab2
quanhn4002 <quannhph36246@fpt.edu.vn>
2023-11-23 22:12:52 +0700

    del footer cu

---
+++ footer2.php
@@ -0,0 +1,61 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .footer1 {
+      background-color: rgb(59, 62, 71);
+      height: 300px;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .text {
+      width: 80%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+    }
+  </style>
+</head>
+
+<body>
+  <footer class="footer1">
+    <div class="text">
+      <div class="text1">
+        <h5>Về Chúng Tôi</h5>
+        <p>Nhà kim hoàn hàng đầu từ năm 19854, cung cấp sự lựa chọn tinh tế nhất của vàng và đá quý cho mọi dịp.</p>
+      </div>
+      <div class="text1">
+        <h5>Liên Kết Nhanh</h5>
+        <ul class="list-unstyled">
+          <li><a href="/products" class="link-light">Sản Phẩm</a></li>
+          <li><a href="/about" class="link-light">Về Chúng Tôi</a></li>
+          <li><a href="/contact" class="link-light">Liên Hệ</a></li>
+          <li><a href="/faq" class="link-light">Câu Hỏi Thường Gặp</a></li>
+        </ul>
+      </div>
+      <div class="text1">
+        <h5>Thông Tin Liên Hệ</h5>
+        <p>16 Trịnh Văn Bô, Nam Từ Liêm, Hà Nội<br>Điện thoại: (123) 642487282<br>Email: tiemvangso6@fpt.edu.vn</p>
+      </div>
+      <div class="text1">
+        <h5>Theo Dõi Chúng Tôi</h5>
+        <a href="#" class="text-light"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="text-light ms-2"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="text-light ms-2"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="text-light ms-2"><i class="fab fa-youtube"></i></a>
+      </div>
+    </div>a
+
+  </footer>
+</body>
+
+</html>
+
+<!--  JS-->
