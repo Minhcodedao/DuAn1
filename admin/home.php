1c8bdfc401433d2b852e7394070b16db866daea7
quanhn4002 <quannhph36246@fpt.edu.vn>
2023-11-23 15:40:22 +0700

    up

--- admin/home.php
+++ admin/home.php
@@ -1,3 +1,8 @@
-<h1 >
-    Chào mừng đến với trang quản trị viên
-</h1>
+<div class="tb">
+    <table border="none">
+        <td>
+             <th><H1>Chào Mừng Bạn Đến Với Trang Quản Trị</H1> </th>
+        </td>
+    </table>
+
+</div>
